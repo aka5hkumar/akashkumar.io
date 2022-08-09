8eeb36c0b0ed0f61bcdf1b6717af22b8e0067e9b
Akash Kumar <akashkumar88@gmail.com>
2022-08-09 19:44:33 -0400

    initial docs

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/akash/Documents/Development/Docusaurus/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/akash/Documents/Development/Docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/akash/Documents/Development/Docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/akash/Documents/Development/Docusaurus/src/css/custom.css'),
+];
